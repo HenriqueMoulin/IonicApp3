4c60212e1d23f250d0bd3249f9663429c65a8f0a
arqdsis <arqdsis@BUTWKSLI7350.animaedu.intranet>
2018-10-09 20:56:35 -0300

    Versao 1 0910

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class HomePage {
+  public lista_filmes = new Array<any>();
 
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController,
+    private movieProvider: MovieProvider) {
 
   }
 
+  ionViewDidLoad() {
+    this.movieProvider.getPopularMovies().subscribe(
+      data => {
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+        console.log(objeto_retorno);
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
 }
